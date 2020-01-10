d92b41b471c30a00d874c300b856b058d7630d50
Chris Hough <tech@aboutchrishough.com>
2020-01-09 21:01:00 -0700

    nav items stubbed

--- source/assets/javascripts/vendor/fontawesome.js
+++ source/assets/javascripts/vendor/fontawesome.js
@@ -1,9 +1,11 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
 
 import { faThumbsUp } from '@fortawesome/free-regular-svg-icons/faThumbsUp';
-
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
 import { faBars } from '@fortawesome/free-solid-svg-icons/faBars';
 
-library.add(faBars, faThumbsUp);
+library.add(faBars,
+            faEnvelope,
+            faThumbsUp);
 
 dom.watch();
