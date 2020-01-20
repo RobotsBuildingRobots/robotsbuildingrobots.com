184119d67845b65863cbc2065c7f24c0824a1926
Chris Hough <tech@aboutchrishough.com>
2020-01-20 14:53:22 -0700

    add hack to cover ios with min height 100vh

--- source/assets/javascripts/pages/index.js
+++ source/assets/javascripts/pages/index.js
@@ -1,3 +1,8 @@
 export default () => {
-  console.log('TEST INDEX!');
+  const $sectionCallToAction = $('.section-call-to-action');
+  const $iOS = navigator.userAgent.match(/Mac/) && navigator.maxTouchPoints && navigator.maxTouchPoints > 2;
+
+	if ($iOS) {
+    $sectionCallToAction.addClass('section-call-to-action-ios')
+	}
 };
