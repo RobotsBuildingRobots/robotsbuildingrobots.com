cd0835522ec5d97a0a814120c70373fd51b6d4d5
Chris Hough <tech@aboutchrishough.com>
2020-01-22 08:41:16 -0700

    fix css linter errors

--- source/assets/javascripts/pages/index.js
+++ source/assets/javascripts/pages/index.js
@@ -2,7 +2,7 @@ export default () => {
   const $sectionCallToAction = $('.section-call-to-action');
   const $iOS = navigator.userAgent.match(/Mac/) && navigator.maxTouchPoints && navigator.maxTouchPoints > 2;
 
-	if ($iOS) {
-    $sectionCallToAction.addClass('section-call-to-action-ios')
-	}
+  if ($iOS) {
+    $sectionCallToAction.addClass('section-call-to-action-ios');
+  }
 };
