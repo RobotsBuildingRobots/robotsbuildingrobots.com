8abf8b4941716daa626406c26c77a6af492d4afb
Chris Hough <tech@aboutchrishough.com>
2020-02-11 22:29:06 -0700

    products mobile interface and svgs

--- source/assets/javascripts/pages/products.js
+++ source/assets/javascripts/pages/products.js
@@ -1,1 +1,8 @@
-export default () => {};
+export default () => {
+  const $sectionProductsWrapper = $('.section-products-wrapper');
+  const $iOS = navigator.userAgent.match(/Mac/) && navigator.maxTouchPoints && navigator.maxTouchPoints > 2;
+
+  if ($iOS) {
+    $sectionProductsWrapper.addClass('section-products-wrapper-ios');
+  }
+};
