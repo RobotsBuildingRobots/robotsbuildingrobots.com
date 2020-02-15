cc255cf4ce36e8300c8af7206480ede4e249f7f5
Chris Hough <tech@aboutchrishough.com>
2020-02-15 10:13:34 -0700

    remote page updates, adjust side xxxl bkgds

--- source/assets/javascripts/vendor/fontawesome.js
+++ source/assets/javascripts/vendor/fontawesome.js
@@ -2,12 +2,15 @@ import { library, dom } from '@fortawesome/fontawesome-svg-core';
 
 import { faThumbsUp } from '@fortawesome/free-solid-svg-icons/faThumbsUp';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
+import { faDesktop } from '@fortawesome/free-solid-svg-icons/faDesktop';
 import { faBars } from '@fortawesome/free-solid-svg-icons/faBars';
 import { faProjectDiagram } from '@fortawesome/free-solid-svg-icons/faProjectDiagram';
 import { faHammer } from '@fortawesome/free-solid-svg-icons/faHammer';
 import { faBroadcastTower } from '@fortawesome/free-solid-svg-icons/faBroadcastTower';
 import { faDollarSign } from '@fortawesome/free-solid-svg-icons/faDollarSign';
 import { faHandHoldingHeart } from '@fortawesome/free-solid-svg-icons/faHandHoldingHeart';
+import { faBrain } from '@fortawesome/free-solid-svg-icons/faBrain';
+import { faSave } from '@fortawesome/free-solid-svg-icons/faSave';
 
 library.add(faBars,
   faEnvelope,
@@ -17,6 +20,9 @@ library.add(faBars,
   faBroadcastTower,
   faHammer,
   faHandHoldingHeart,
+  faDesktop,
+  faBrain,
+  faSave,
   faThumbsUp);
 
 dom.watch();
