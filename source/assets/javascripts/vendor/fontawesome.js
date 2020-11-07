879ad233db09829fe066a453032d87e671ae7ab7
Chris Hough <tech@aboutchrishough.com>
2020-11-07 15:27:09 -0700

    update bio, add header phone

--- source/assets/javascripts/vendor/fontawesome.js
+++ source/assets/javascripts/vendor/fontawesome.js
@@ -4,11 +4,13 @@ import { faBars } from '@fortawesome/free-solid-svg-icons/faBars';
 import { faProjectDiagram } from '@fortawesome/free-solid-svg-icons/faProjectDiagram';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
 import { faBrain } from '@fortawesome/free-solid-svg-icons/faBrain';
+import { faPhoneSquare } from '@fortawesome/free-solid-svg-icons/faPhoneSquare';
 
 library.add(faBars,
             faBrain,
             faEnvelope,
             faProjectDiagram,
+            faPhoneSquare,
             faHammer);
 
 dom.watch();
