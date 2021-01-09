bcaa929a6b808b28be724331a71e9ac6ae5f8677
Chris Hough <tech@aboutchrishough.com>
2021-01-08 22:24:33 -0700

    desktop

--- source/assets/javascripts/pages/contact.js
+++ source/assets/javascripts/pages/contact.js
@@ -1,3 +1,92 @@
 export default () => {
+  const $contactForm = $('#contact-form');
 
+  $.validator.addMethod('alphanumeric', function (value, element) { // eslint-disable-line func-names
+    return this.optional(element) || /^[a-z0-9!?\-\s]+$/i.test(value);
+  });
+
+  $.validator.setDefaults({
+    submitHandler() {
+      const $socialEmail = 'sales@robotsbuildingrobots.com';
+      const $inputFirstName = $('#input-first-name').val();
+      const $inputLastName = $('#input-last-name').val();
+      const $inputEmail = $('#input-email').val();
+      const $inputEmailBody = $('#input-email-body').val();
+      const $inputSubject = $('#input-subject').val();
+
+      let $inputBody = `Subject: ${$inputSubject}`;
+      $inputBody = `${$inputBody}\r\nFirst Name: ${$inputFirstName}`;
+      $inputBody = `${$inputBody}\r\nLast Name: ${$inputLastName}`;
+      $inputBody = `${$inputBody}\r\nEmail Address: ${$inputEmail}`;
+      $inputBody = `${$inputBody}\r\n\r\n--------------------------------------------`;
+      $inputBody = `${$inputBody}\r\n\r\n${$inputEmailBody}`;
+
+      window.location.href = `mailto:${encodeURIComponent($socialEmail)}?subject=${encodeURIComponent($inputSubject)}&body=${encodeURIComponent($inputBody)}`;
+    },
+  });
+
+  $contactForm.validate({
+    rules: {
+      'input-first-name': {
+        required: true,
+        minlength: 3,
+        alphanumeric: true,
+        normalizer(value) { return $.trim(value); },
+      },
+      'input-last-name': {
+        required: true,
+        minlength: 3,
+        alphanumeric: true,
+        normalizer(value) { return $.trim(value); },
+      },
+      'input-email': {
+        required: true,
+        minlength: 3,
+        email: true,
+        normalizer(value) { return $.trim(value); },
+      },
+      'input-subject': {
+        required: true,
+        minlength: 3,
+        alphanumeric: true,
+        normalizer(value) { return $.trim(value); },
+      },
+      'input-email-body': {
+        required: true,
+        minlength: 3,
+        normalizer(value) { return $.trim(value); },
+      },
+    },
+    messages: {
+      'input-first-name': {
+        required: 'You must enter a first name!',
+        minlength: 'Three character minimum!',
+        alphanumeric: 'Only alphabetical, numbers, spaces, and dash characters!',
+      },
+      'input-last-name': {
+        required: 'You must enter a last name!',
+        minlength: 'Three character minimum!',
+        alphanumeric: 'Only alphabetical, numbers, spaces, and dash characters!',
+      },
+      'input-email': {
+        required: 'You must enter an email address!',
+        minlength: 'Three character minimum!',
+        email: 'Not a valid email address!',
+      },
+      'input-subject': {
+        required: 'You must enter a title!',
+        minlength: 'Three character minimum!',
+        alphanumeric: 'Only alphabetical, numbers, spaces, and dash characters!',
+      },
+      'input-email-body': {
+        required: 'You must enter details!',
+        minlength: 'Three character minimum!',
+      },
+    },
+    errorElement: 'div',
+    errorPlacement(error, element) {
+      error.addClass('invalid-feedback');
+      error.insertAfter(element);
+    },
+  });
 };
