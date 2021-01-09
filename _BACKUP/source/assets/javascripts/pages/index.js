d04d723c17d061e9786d9b2c87aa38d208a6832c
Chris Hough <tech@aboutchrishough.com>
2021-01-08 22:39:31 -0700

    purge backup assets

--- _BACKUP/source/assets/javascripts/pages/index.js
+++
@@ -1,8 +0,0 @@
-export default () => {
-  const $sectionCallToAction = $('.section-call-to-action');
-  const $iOS = navigator.userAgent.match(/Mac/) && navigator.maxTouchPoints && navigator.maxTouchPoints > 2;
-
-  if ($iOS) {
-    $sectionCallToAction.addClass('section-call-to-action-ios');
-  }
-};
--- _BACKUP/source/assets/javascripts/vendor.js
+++
@@ -1,9 +0,0 @@
-window.jQuery = require('jquery');
-
-window.$ = window.jQuery;
-window.Popper = require('popper.js');
-
-require('bootstrap');
-require('breakpoints');
-require('validation');
-require('fontawesome');
