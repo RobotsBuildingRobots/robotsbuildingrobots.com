f051c6c967dca070609fa2d21b8e8bd94dd8e8d4
Chris Hough <tech@aboutchrishough.com>
2025-11-30 20:48:19 -0800

    fix site host

--- lib/build_process_functions.rb
+++ lib/build_process_functions.rb
@@ -3,7 +3,7 @@
 def adjust_final_source
   Dir.glob('build/assets/*.json').select { |file| file.include?('manifest') }.each { |file| File.delete(file) }
 
-  File.write('build/CNAME', 'links.aboutchrishough.com')
+  File.write('build/CNAME', ENV['SITE_HOST'])
 
   if File.exist?('build/four-o-four/index.html')
     FileUtils.cp('build/four-o-four/index.html', 'build/404.html')
